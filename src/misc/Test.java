c0f013c96d86d893b2c695db5c4ff4268a87e06a
Luke Broussard <brouslr0@sewanee.edu>
2026-01-16 15:50:25 +0000

    configured the algs4 library

---
+++ src/misc/Test.java
@@ -0,0 +1,7 @@
+import edu.princeton.cs.algs4.*; // The * imports everything in the library
+
+public class Test {
+    public static void main(String[] args) {
+        StdOut.println("Hello"); // Clean and simple
+    }
+}
