3d0f30106ab62aff2fa5c38f5a28f8390ef34b55
Luke Broussard <brouslr0@sewanee.edu>
2026-01-23 05:04:58 +0000

    finished hw1 plus added extra algorithms

--- src/homeworks/hw1/StackWhileLoop.java
+++ src/homeworks/hw1/StackWhileLoop.java
@@ -7,7 +7,9 @@ public class StackWhileLoop {
     public static void main(String[] args) {
 
         Stack<String> stk = new Stack<String>();
-
+        
+        // reference I used https://www.geeksforgeeks.org/java/stack-iterator-method-in-java-with-example/
+        
         Iterator<String> cnt = stk.iterator(); 
 
         while (cnt.hasNext()) {
